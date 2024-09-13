e1d51ee895b0159c457391b60302b24dc79de383
godoro1 <162140530+godoro1@users.noreply.github.com>
2024-09-13 10:47:21 -0300

    z

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'matematica',
+    'quanto é 150+1388?',
+    '1538'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz dia em Inglês?',
+    'dia em ingles é day'
+)
