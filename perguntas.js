a817210ce8ee3311c0d62b7908b58841d27602f3
godoro1 <162140530+godoro1@users.noreply.github.com>
2024-10-28 11:44:19 -0300

    z

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Portugues',
+    'oque é verbo',
+    'O verbo é a palavra que expressa ações, estados e fenômenos da natureza.'
 )
 
 criaCartao(
@@ -14,4 +14,38 @@ criaCartao(
     'Lingua inglesa',
     'Como se diz dia em Inglês?',
     'dia em ingles é day'
+)
+criaCartao(
+    'Portugues',
+    'oque é adjetivo',
+    'O adjetivo é uma classe de palavras que atribui características aos substantivos, ou seja, ele indica suas qualidades e estados.'
+)
+
+criaCartao(
+    'matematica',
+    'quanto é 150+13?',
+    '163'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'oi em ingles é hi'
+)
+criaCartao(
+    'Portugues',
+    'O que é sujeito',
+    'O sujeito é o elemento que pratica ou sofre a ação expressa pelo verbo de uma oração.'
+)
+
+criaCartao(
+    'matematica',
+    'quanto é 150+150?',
+    '300'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz vida em Inglês?',
+    'vida em ingles é life'
 )
